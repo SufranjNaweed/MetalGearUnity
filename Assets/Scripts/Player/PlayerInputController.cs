590748f50ee7a470eea4744df330536c6dae2127
SufranjNaweed <sufranj.naweed@gmail.com>
2021-03-12 16:25:17 +0100

    base project

---
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace StealthGame
+{
+    #region ButtonInput struct
+
+    public struct ButtonInput
+    {
+        #region Public properties
+
+        public bool IsActive { get; }
+        public bool IsDown { get; }
+        public bool IsUp { get; }
+        public bool IsDoubleTap { get; }
+        public float Value { get; }
+
+        #endregion
+
+
+        #region Constructors
+
+        public ButtonInput(bool isActive, bool isDown, bool isUp, bool isDoubleTap, float value)
+        {
+            IsActive = isActive;
+            IsDown = isDown;
+            IsUp = isUp;
+            IsDoubleTap = isDoubleTap;
+            Value = value;
+        }
+
+        #endregion
+    }
+
+    #endregion
+
+    public class PlayerInputController : MonoBehaviour
+    {
+        #region Show in inspector
+
+        [SerializeField] private float _inputThreshold;
+        [SerializeField] private float _doubleTapDelay;
+
+        #endregion
+
+
+        #region Public properties
+
+        public ButtonInput HorizontalInput { get; private set; }
+        public ButtonInput VerticalInput { get; private set; }
+        public Vector3 MovementInput { get; private set; }
+        public bool HasMovementInput { get; private set; }
+        public ButtonInput SneakInput { get; private set; }
+        public ButtonInput RunInput { get; private set; }
+        public ButtonInput JumpInput { get; private set; }
+
+        #endregion
+
+
+        #region Update
+
+        private void Update()
+        {
+            HorizontalInput = GetAxisInput("Horizontal");
+            VerticalInput = GetAxisInput("Vertical");
+
+            HasMovementInput = HorizontalInput.IsActive || VerticalInput.IsActive;
+
+            MovementInput = new Vector3()
+            {
+                x = HorizontalInput.Value,
+                y = 0,
+                z = VerticalInput.Value
+            };
+
+            MovementInput = Vector3.ClampMagnitude(MovementInput, 1);
+
+            SneakInput = GetButtonInput("Sneak");
+            RunInput = GetButtonInput("Run");
+            JumpInput = GetButtonInput("Jump");
+     
+        }
+
+        #endregion
+
+
+        #region Private methods
+
+        private ButtonInput GetButtonInput(string buttonName)
+        {
+            bool isDoubleTap = GetDoubleTapStatus(buttonName, Input.GetButtonDown(buttonName));
+
+            return new ButtonInput(
+                Input.GetButton(buttonName),
+                Input.GetButtonDown(buttonName),
+                Input.GetButtonUp(buttonName),
+                isDoubleTap,
+                Input.GetButton(buttonName) ? 1 : 0);
+        }
+
+        private ButtonInput GetAxisInput(string axisName)
+        {
+            bool isActive = IsAxisActive(axisName);
+
+            bool isDown = false;
+            bool isUp = false;
+            if (!_axisInputLastFrame.TryGetValue(axisName, out bool wasActive))
+            {
+                _axisInputLastFrame.Add(axisName, false);
+                wasActive = false;
+            }
+
+            if (wasActive && !isActive)
+            {
+                isUp = true;
+            }
+            if (!wasActive && isActive)
+            {
+                isDown = true;
+            }
+
+            bool isDoubleTap = GetDoubleTapStatus(axisName, isDown);
+
+            return new ButtonInput(
+                isActive,
+                isDown,
+                isUp,
+                isDoubleTap,
+                Input.GetAxisRaw(axisName));
+        }
+
+        private bool GetDoubleTapStatus(string name, bool isDown)
+        {
+            bool isDoubleTap = false;
+            if (_doubleTapTimes.TryGetValue(name, out float doubleTapTime))
+            {
+                if (isDown)
+                {
+                    isDoubleTap = Time.time < doubleTapTime;
+                    doubleTapTime = Time.time + _doubleTapDelay;
+                    _doubleTapTimes[name] = doubleTapTime;
+                }
+            }
+            else
+            {
+                isDoubleTap = false;
+                doubleTapTime = Time.time + _doubleTapDelay;
+                _doubleTapTimes.Add(name, doubleTapTime);
+            }
+
+            return isDoubleTap;
+        }
+
+        private bool IsAxisActive(string axisName)
+        {
+            return Mathf.Abs(Input.GetAxisRaw(axisName)) > _inputThreshold;
+        }
+
+        #endregion
+
+
+        #region Private
+
+        private Dictionary<string, float> _doubleTapTimes = new Dictionary<string, float>();
+        private Dictionary<string, bool> _axisInputLastFrame = new Dictionary<string, bool>();
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,104 @@
+﻿
+using UnityEngine;
+
+namespace StealthGame
+{
+    [SelectionBase]
+    public class PlayerMovementController : MonoBehaviour
+    {
+        #region Show in inspector
+
+        [Header("Controllers")]
+
+        [SerializeField] private PlayerInputController _inputController;
+
+        [Header("Movement parameters")]
+        [SerializeField] private float _walkSpeed;
+        [SerializeField] private float _runSpeed;
+        [SerializeField] private float _sneakSpeed;
+        [SerializeField] private float _turnSpeed;
+
+        #endregion
+
+
+        #region Init
+
+        private void Awake()
+        {
+            _transform = GetComponent<Transform>();
+            _rigidbody = GetComponent<Rigidbody>();
+            _cameraTransform = Camera.main.transform;
+        }
+
+
+        #endregion
+
+
+        #region Update
+
+        private void Update()
+        {
+            _movementDirection = _transform.TransformDirection(_inputController.MovementInput);
+            _movementDirection.y = 0;
+
+            SetSpeed();
+        }
+
+        private void SetSpeed()
+        {
+            if (_inputController.SneakInput.IsActive)
+            {
+                _currentSpeed = _sneakSpeed;
+            }
+            else if (_inputController.RunInput.IsActive)
+            {
+                _currentSpeed = _runSpeed;
+            }
+            else
+            {
+                _currentSpeed = _walkSpeed;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            // Déplace le joueur selon son référentiel
+            Move();
+
+            // Tourne le joueur vers l'orientation de la caméra
+            RotateTowardsCameraForward();
+        }
+
+        private void Move()
+        {
+            Vector3 velocity = _movementDirection * _currentSpeed;
+            _rigidbody.velocity = velocity;
+        }
+
+        private void RotateTowardsCameraForward()
+        {
+            if (_inputController.HasMovementInput)
+            {
+                Vector3 lookDirection = _cameraTransform.forward;
+                lookDirection.y = 0;
+                Quaternion rotation = Quaternion.LookRotation(lookDirection);
+                rotation = Quaternion.RotateTowards(_rigidbody.rotation, rotation, _turnSpeed * Time.fixedDeltaTime);
+                _rigidbody.MoveRotation(rotation);
+            }
+        }
+
+        #endregion
+
+
+        #region Private
+
+        private Transform _transform;
+        private Transform _cameraTransform;
+        private Rigidbody _rigidbody;
+
+        private Vector3 _movementDirection;
+        private float _currentSpeed;
+
+        #endregion
+    }
+}
